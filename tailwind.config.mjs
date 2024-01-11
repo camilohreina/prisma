2f78ec93a862b99b5d4fb6ce79f6f9b3bc3577f8
Camilo Hernandez <jhernandez@brangerbriz.com>
2024-01-10 19:57:33 -0500

    Add background color and navbar component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: ['class', '[data-mode="dark"]'],
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
         fontFamily: {
             sans: ['Poppins', 'sans-serif'],
+        },
+        container: {
+            padding: {
+                DEFAULT: '1rem',
+                sm: '2rem',
+                lg: '4rem',
+                xl: '5rem',
+                '2xl': '6rem',
+            },
         },
         extend: {},
     },
